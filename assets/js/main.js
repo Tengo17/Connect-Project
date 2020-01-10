c05904532939ed6a6bdc8858f5b49c28bb3d46ae
Tengo17 <54151792+Tengo17@users.noreply.github.com>
2020-01-10 23:37:56 +0400

    language bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,28 @@
-$(document).ready(function() {
-      setTimeout(function() {
-        $('#bigloading').addClass('ready');
-      }, 3500);
-});
+// preload 
+document.body.onload = function(){
+  setTimeout(function() {
+    var preloader = document.getElementById('bigloading');
+    if( !preloader.classList.contains('ready') )
+    {
+      preloader.classList.add('ready');
+    }
+  }, 3500)
+}
+
+// dropdown 
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
